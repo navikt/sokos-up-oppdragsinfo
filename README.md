b806e5d3248ff3e35d7fa956c409369d42d131e6
takwaiwang <tak.wai.wang@nav.no>
2024-08-16 13:57:55 +0200

    Oppdater vite.config oppsett og åpen mulighet til å test mot test-miljø

--- README.md
+++ README.md
@@ -8,6 +8,23 @@
 4. Start appen lokalt `pnpm run dev` (mock) eller mot backend lokalt `pnpm run dev:backend` [sokos-oppdrag](https://github.com/navikt/sokos-oppdrag)
 5. Appen nås på http://localhost:5173
 
+### Start appen mot test miljøet
+
+Endre vite.config.ts server.proxy innstilling med:
+
+```javascript
+proxy: {
+   "/oppdrag-api/api/v1": {
+   target: "https://sokos-oppdrag.dev-fss-pub.nais.io",
+   rewrite: (path: string) => path.replace(/^\/oppdrag-api/, ""),
+   changeOrigin: true,
+   secure: true
+  }
+}
+```
+
+Start applikasjon med `pnpm run dev:backend` som backend loaklt.
+
 NB! Anbefaler sette opp [ModHeader](https://modheader.com/) extension på Chrome for å sende med Obo-token i `Authorization` header når du kjører mot backend lokalt da den krever at token inneholder NavIdent.
 
 # Henvendelser
--- vite.config.ts
+++ vite.config.ts
@@ -1,69 +1,65 @@
-import { defineConfig } from "vite";
+/// <reference types="vitest" />
 import importMapPlugin from "@eik/rollup-plugin";
 import terser from "@rollup/plugin-terser";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { defineConfig } from "vite";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-import EnvironmentPlugin from "vite-plugin-environment";
 import { viteMockServe } from "vite-plugin-mock";
 
 const reactUrl = "https://www.nav.no/tms-min-side-assets/react/18/esm/index.js";
 const reactDomUrl =
   "https://www.nav.no/tms-min-side-assets/react-dom/18/esm/index.js";
 
-export default defineConfig(({ command }) => {
-  const isDev = command === "serve" && process.env.NODE_ENV === "development-local";
-
-  return {
-    plugins: [
-      react(),
-      cssInjectedByJsPlugin(),
-      EnvironmentPlugin({
-        NODE_ENV: process.env.NODE_ENV || "development"
-      }),
-      viteMockServe({
-        mockPath: "mock",
-        enable: !isDev
-      }),
-      {
-        ...importMapPlugin({
-          maps: [
-            {
-              imports: {
-                react: reactUrl,
-                "react-dom": reactDomUrl
-              }
-            }
-          ]
-        }),
-        enforce: "pre",
-        apply: "build"
-      },
-      terser()
-    ],
-    base: "/oppdragsinfo",
-    build: {
-      lib: {
-        entry: resolve(__dirname, "src/App.tsx"),
-        name: "sokos-up-oppdragsinfo",
-        formats: ["es"],
-        fileName: () => "bundle.js"
-      }
+export default defineConfig(({ mode }) => ({
+  base: "/oppdragsinfo",
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/App.tsx"),
+      name: "sokos-up-oppdragsinfo",
+      formats: ["es"],
+      fileName: () => "bundle.js",
     },
-    css: {
-      modules: {
-        generateScopedName: "[name]__[local]___[hash:base64:5]"
-      }
+  },
+  css: {
+    modules: {
+      generateScopedName: "[name]__[local]___[hash:base64:5]",
     },
-    server: {
-      proxy: isDev
-        ? {
-          "/oppdrag-api/api/v1": {
-            target: "http://localhost:8080",
-            rewrite: (path) => path.replace(/^\/oppdrag-api/, "")
-          }
+  },
+  server:
+    mode == "local-dev"
+      ? {
+          proxy: {
+            "/oppdrag-api/api/v1": {
+              target: "http://localhost:8080",
+              rewrite: (path: string) => path.replace(/^\/oppdrag-api/, ""),
+              changeOrigin: true,
+              secure: false,
+            },
+          },
         }
-        : {}
-    }
-  };
-});
+      : {},
+  plugins: [
+    react(),
+    cssInjectedByJsPlugin(),
+    viteMockServe({
+      mockPath: "mock",
+      enable: mode === "local-mock",
+    }),
+    {
+      ...importMapPlugin({
+        maps: [
+          {
+            imports: {
+              react: reactUrl,
+              "react-dom": reactDomUrl,
+            },
+          },
+        ],
+      }),
+      enforce: "pre",
+      apply: "build",
+    },
+    terser(),
+  ],
+}));
