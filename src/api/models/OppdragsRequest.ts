a3c3e5ede385d3d984985b87d2504f4fe22d2a06
Lars Hartvigsen <lars.hartvigsen@nav.no>
2024-08-27 12:55:55 +0200

    type -> kode

--- src/api/models/OppdragsRequest.ts
+++ src/api/models/OppdragsRequest.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const OppdragsRequestSchema = z.object({
   gjelderId: z.string(),
-  faggruppeType: z.string().optional()
+  faggruppeKode: z.string().optional(),
 });
 
-export type OppdragsRequest = z.infer<typeof OppdragsRequestSchema>
+export type OppdragsRequest = z.infer<typeof OppdragsRequestSchema>;
