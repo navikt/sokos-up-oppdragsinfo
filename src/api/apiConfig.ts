03d0daff518a7ab846deff907d6e45b37049e0d5
mudasar187 <mudasar_cheema@hotmail.com>
2024-09-11 11:26:13 +0200

    tester å kaste feil oppover

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,5 +1,4 @@
 import axios, { CreateAxiosDefaults } from "axios";
-import { ApiError, HttpStatusCodeError } from "../types/errors";
 
 const config = (baseUri: string): CreateAxiosDefaults => ({
   baseURL: baseUri,
@@ -16,20 +15,6 @@ const config = (baseUri: string): CreateAxiosDefaults => ({
 function api(baseUri: string) {
   const instance = axios.create(config(baseUri));
 
-  instance.interceptors.response.use(
-    (response) => response,
-    (error) => {
-      if (error.response?.status === 400) {
-        // her kan vi legge feilkoder også som vi fra backend
-        throw new HttpStatusCodeError(error.response?.status);
-      }
-      if (error.response?.status === 401 || error.response?.status === 403) {
-        // Uinnlogget - vil ikke skje i miljø da appen er beskyttet
-        return Promise.reject(error);
-      }
-      throw new ApiError("Issues with connection to backend");
-    },
-  );
   return instance;
 }
 
