0a3732ae2be31ac8b480d6400ea5bf3ed0544faf
Lars Hartvigsen <lars.hartvigsen@nav.no>
2025-05-09 09:05:10 +0200

    fikser avhengigheter

--- src/pages/SokPage.tsx
+++ src/pages/SokPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 import { Alert, Heading } from "@navikt/ds-react";
 import { hentOppdrag } from "../api/apiService";
 import { useStore } from "../store/AppState";
